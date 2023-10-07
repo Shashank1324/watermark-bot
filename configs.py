d657971dedb555c9acde5f039a3816243a219729
Shashank1324 <87693399+Shashank1324@users.noreply.github.com>
2023-10-08 00:02:47 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -32,8 +32,7 @@ Hi, I am Video Watermark Adder Bot!
 __Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/).__
 
 Desgined by @Dads_links 
- """ 
-        PROGRESS = """<b>\n   
+ """ PROGRESS = """<b>\n   
   ╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣  
   ┣⪼ 📊 Percentage: {0}%  
   ┣⪼ ✅ Dᴏɴᴇ : {1}  
